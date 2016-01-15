ca68a60c8f1338c75f02286f8f1c6096f41cfa1f
Alan Ferrari <alan.ferrari@supsi.ch>
2016-01-15 14:15:19 +0100

    BT (2)

--- android/app/src/main/java/ch/supsi/dti/isin/obd/OBDActivity.java
+++ android/app/src/main/java/ch/supsi/dti/isin/obd/OBDActivity.java
@@ -51,7 +51,7 @@ public class OBDActivity extends Activity {
             mService = binder.getService();
             mBound = true;
 
-           // serviceConnected();
+            serviceConnected();
         }
 
         @Override
